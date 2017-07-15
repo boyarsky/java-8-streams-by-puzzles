55c1f083e35d9f213e02d9620205c4950269f64e
Jeanne Boyarsky <nyjeanne@gmail.com>
2017-07-15 19:57:19 -0400

    update from presenting this to FIRST alum and CodeRanch mod

---
+++ src/MoreLogic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MoreLogic {
+
+	public static void main(String[] args) {
+		List<String> matchingList = Arrays.asList(
+	        "calf", "dolphin", "manatee", "ocra", "walrus", "whale");
+		printMe(matchingList);
+	}
+	private static void printMe(List<String> list) {
+	 // Print the third distinct element alphabetically 
+	 // that is over 4 characters long
+	}
+
+}
---
+++ src/OddsAndEvens.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class OddsAndEvens {
+
+	public static void main(String[] args) {
+		System.out.println(split(Arrays.asList(1, 4, 8, 13)));
+		System.out.println(split(Arrays.asList(1, 3)));
+
+	}
+
+	private static Map<Boolean, List<Integer>> split(List<Integer> list) {
+		return new HashMap<>();
+	}
+}
